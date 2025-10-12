8e51644640dd659069c91056fe785a9f5d926bbe
CmdCtrlDevic3 <cmdctrldevic3@proton.me>
2025-10-12 22:11:10 +0300

    r8s: Update PDA version

--- lineage_r8s.mk
+++ lineage_r8s.mk
@@ -37,10 +37,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="r8sxx-user 11 RP1A.200720.012 G780FXXSDFWH8 release-keys" \
-    BuildFingerprint=samsung/r8sxx/r8s:11/RP1A.200720.012/G780FXXSDFWH8:user/release-keys \
+    BuildDesc="r8sxx-user 11 RP1A.200720.012 G780FXXSNFYH1 release-keys" \
+    BuildFingerprint=samsung/r8sxx/r8s:11/RP1A.200720.012/G780FXXSNFYH1:user/release-keys \
     DeviceProduct=r8sxx
 
 # Define PDA property for camera
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.build.PDA=G780FXXSDFWH8
+    ro.build.PDA=G780FXXSNFYH1
