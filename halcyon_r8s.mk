6cc01fffdcd116c64cbd2fd6ee7d3f388cfbd41a
CmdCtrlDevic3 <cmdctrldevic3@proton.me>
2026-04-10 10:12:45 +0300

    r8s: Init halcyon
    Signed-off-by: CmdCtrlDevic3 <cmdctrldevic3@proton.me>

--- halcyon_r8s.mk
+++ halcyon_r8s.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/samsung/r8s/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := r8s
-PRODUCT_NAME := lineage_r8s
+PRODUCT_NAME := halcyon_r8s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G780F
 PRODUCT_MANUFACTURER := samsung
